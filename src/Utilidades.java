4208a5e44eae66f7636af70fcd802aa783c11303
tarea-ch2hw1-juego-de-cartas-21-JorgePucha created by GitHub Classroom <33530657+JorgePucha@users.noreply.github.com>
2017-12-10 20:52:40 -0500

    Update and rename PreguntaAlmacenadas.java to Utilidades.java

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -1,15 +1,22 @@
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 /**
  *
  * @author goquezadag
  */
-public class PreguntaAlmacenadas {
+public class Utilidades {
 
     public static boolean validarUsuario(ArrayList<PreguntasTerminadas> listaPreguntas, String ced) {
         boolean res = false;
-
         for (PreguntasTerminadas elemento : listaPreguntas) {
             if (elemento.getCedula().equals(ced)) {
                 res = true;
@@ -17,25 +24,38 @@ public class PreguntaAlmacenadas {
         }
         return res;
     }
-
     public static void presentarPreguntas(ArrayList<PreguntasTerminadas> listaPreguntas) {
+        String aux = "";
         String ced = JOptionPane.showInputDialog(null, "Ingrese Cedula para mostrar Examen", "UNL", JOptionPane.QUESTION_MESSAGE);
         boolean act = validarUsuario(listaPreguntas, ced);
         if (act) {
-            String aux = "";
+            aux = "";
             for (PreguntasTerminadas elemento : listaPreguntas) {
                 if (elemento.getCedula().equals(ced)) {
-                    aux = aux + elemento.toString() + "\n";
+                    aux = aux + elemento.toString() + ",Puntuancion Total: " + calificarPreguntas2(listaPreguntas, ced) + "\n";
                 }
-
             }
             JOptionPane.showMessageDialog(null, "Resultado Examen:" + ced + "\n" + aux, "UNL", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Usuario no existe", "UNL", JOptionPane.ERROR_MESSAGE);
         }
-
     }
-
+    public static void guardarPreguntas(ArrayList<PreguntasTerminadas> listaPreguntas) {
+        try {
+            File file = new File("Archivo.txt");
+            FileWriter escribir = new FileWriter(file, true);
+            String aux = "";
+            PreguntasTerminadas elemento;
+            for (int i = 0; i < listaPreguntas.size(); i++) {
+                elemento = listaPreguntas.get(i);
+                aux = elemento.getCedula();
+                escribir.write(elemento.toString() + ",Puntuancion Total: " + calificarPreguntas2(listaPreguntas, aux) + "\n");
+            }
+            escribir.close();
+        } catch (Exception e) {
+            System.out.println("no se encontro el archivo" + e);
+        }
+    }
     public static void calificarPreguntas(ArrayList<PreguntasTerminadas> listaPreguntas) {
         int calificacion = 0;
         String ced = JOptionPane.showInputDialog(null, "Ingrese Cedula para calificar Examen", "UNL", JOptionPane.QUESTION_MESSAGE);
@@ -50,5 +70,15 @@ public class PreguntaAlmacenadas {
         } else {
             JOptionPane.showMessageDialog(null, "Usuario no existe", "UNL", JOptionPane.ERROR_MESSAGE);
         }
-    }}
+    }
 
+    public static String calificarPreguntas2(ArrayList<PreguntasTerminadas> listaPreguntas, String ced) {
+        int calificacion = 0;
+        for (PreguntasTerminadas elemento : listaPreguntas) {
+            if (elemento.getCedula().equals(ced) && elemento.isContestada() == true) {
+                calificacion++;
+            }
+        }
+        return String.valueOf(calificacion);
+    }
+}
